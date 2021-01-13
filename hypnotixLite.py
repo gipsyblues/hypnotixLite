0a40a44b320cb1b9a00d4fce0bbb06cfeeeb69b7
Axel Schneider <axel99092@t-online.de>
2021-01-13 18:37:14 +0100

    Update hypnotixLite.py
    added: searchfield

--- hypnotixLite.py
+++ hypnotixLite.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
+### based on hypnotix 
+### https://github.com/linuxmint/hypnotix
+
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gtk', '3.0')
@@ -20,8 +24,7 @@ class MyWindow(Gtk.Window):
         
     def my_zoom(self, widget, event):
         if not self.sidebar.is_visible():                  
-            w = self.win.get_size()[0]
-            h = self.win.get_size()[1]
+            w, h = self.win.get_size()
             direction = event.get_scroll_deltas()[2]
             if direction < 0:
                 if not w > 1250:
@@ -100,109 +103,126 @@ class MyWindow(Gtk.Window):
         else:
             return False
             
+    def showHelp(self):
+        help_text = """Shortcuts
+1 to 9 -> load list 1 to 9
+f -> toggle Fullscreen
+Escape -> leave Fullscreen
+s -> toggle Sidebar
+wheel -> Zoo in/out
+q -> Quit
+Key Up -> next Channel
+Key Down -> previous Channel
+Key Plus -> more Volume
+Key Minus -> less Volume
+i -> import m3u
+"""
+        self.msgbox(help_text)
+            
     def on_key_press_event(self, widget, event):
-        if event.keyval == Gdk.KEY_q:
-            Gtk.main_quit()
-        if event.keyval == Gdk.KEY_i:            
-            self.import_playlist()
-        ### lists
-        if event.keyval == Gdk.KEY_1:
-            if fpath.isfile("mychannels1.txt"):
-                self.makeList(self.list_1)
-                self.sidebar.show()
-        if event.keyval == Gdk.KEY_2:
-            if fpath.isfile("mychannels2.txt"):
-                self.makeList(self.list_2)
-                self.sidebar.show()
-        if event.keyval == Gdk.KEY_3:
-            if fpath.isfile("mychannels3.txt"):
-                self.makeList(self.list_3)
-                self.sidebar.show()
-        if event.keyval == Gdk.KEY_4:
-            if fpath.isfile("mychannels4.txt"):
-                self.makeList(self.list_4)
-                self.sidebar.show()
-        if event.keyval == Gdk.KEY_5:
-            if fpath.isfile("mychannels5.txt"):
-                self.makeList(self.list_5)
-                self.sidebar.show()
-        if event.keyval == Gdk.KEY_6:
-            if fpath.isfile("mychannels6.txt"):
-                self.makeList(self.list_6)
-                self.sidebar.show()
-        if event.keyval == Gdk.KEY_7:
-            if fpath.isfile("mychannels7.txt"):
-                self.makeList(self.list_7)
-                self.sidebar.show()
-        if event.keyval == Gdk.KEY_8:
-            if fpath.isfile("mychannels8.txt"):
-                self.makeList(self.list_8)
-                self.sidebar.show()
-        if event.keyval == Gdk.KEY_9:
-            if fpath.isfile("mychannels9.txt"):
-                self.makeList(self.list_9)
-                self.sidebar.show()
-        if event.keyval == Gdk.KEY_0:
-            if fpath.isfile("mychannels9.txt"):
-                self.makeList(self.list_10)
-                self.sidebar.show()
-            # volume up
-        if event.keyval == Gdk.KEY_plus:
-            if self.mpv.volume < 205:
-                self.mpv.volume += 5.0
-                self.volume = self.mpv.volume
-            print(f"Volume: {self.mpv.volume}")
-            # volume down
-        if event.keyval == Gdk.KEY_minus: 
-            if self.mpv.volume >= 5:
-                self.mpv.volume -= 5.0
-                self.volume = self.mpv.volume
-            print(f"Volume: {self.mpv.volume}")
-            # toggle sidebar
-        if event.keyval == Gdk.KEY_s:
-            self.toggleSideBar()            
-        if event.keyval == Gdk.KEY_f or \
-             (self.fullscreen and event.keyval == Gdk.KEY_Escape):
-            self.toggle_fullscreen()
-            # next
-        if self.is_playing:
-            if event.keyval == Gdk.KEY_Up:
-                child = self.channelbox.get_child_at_index(self.id + 1)
-                self.channelbox.select_child(child)
-                if self.sidebar.is_visible():
-                    self.toggleSideBar()
-                self.id += 1
-                self.play_async(self.nameList[self.id], self.urlList[self.id])
-                # previous
-            if event.keyval == Gdk.KEY_Down:
-                child = self.channelbox.get_child_at_index(self.id - 1)
-                self.channelbox.select_child(child)
-                if self.sidebar.is_visible():
-                    self.toggleSideBar()
-                self.id -= 1
-                self.play_async(self.nameList[self.id], self.urlList[self.id]) 
+        if not self.searchbar.has_focus():
+            if event.keyval == Gdk.KEY_F1:
+                self.showHelp()
+            if event.keyval == Gdk.KEY_q:
+                Gtk.main_quit()
+            if event.keyval == Gdk.KEY_i:            
+                self.import_playlist()
+            ### lists
+            if event.keyval == Gdk.KEY_1:
+                if fpath.isfile("mychannels1.txt"):
+                    self.makeList(self.list_1)
+                    self.vbox.show()
+            if event.keyval == Gdk.KEY_2:
+                if fpath.isfile("mychannels2.txt"):
+                    self.makeList(self.list_2)
+                    self.vbox.show()
+            if event.keyval == Gdk.KEY_3:
+                if fpath.isfile("mychannels3.txt"):
+                    self.makeList(self.list_3)
+                    self.vbox.show()
+            if event.keyval == Gdk.KEY_4:
+                if fpath.isfile("mychannels4.txt"):
+                    self.makeList(self.list_4)
+                    self.vbox.show()
+            if event.keyval == Gdk.KEY_5:
+                if fpath.isfile("mychannels5.txt"):
+                    self.makeList(self.list_5)
+                    self.vbox.show()
+            if event.keyval == Gdk.KEY_6:
+                if fpath.isfile("mychannels6.txt"):
+                    self.makeList(self.list_6)
+                    self.vbox.show()
+            if event.keyval == Gdk.KEY_7:
+                if fpath.isfile("mychannels7.txt"):
+                    self.makeList(self.list_7)
+                    self.vbox.show()
+            if event.keyval == Gdk.KEY_8:
+                if fpath.isfile("mychannels8.txt"):
+                    self.makeList(self.list_8)
+                    self.vbox.show()
+            if event.keyval == Gdk.KEY_9:
+                if fpath.isfile("mychannels9.txt"):
+                    self.makeList(self.list_9)
+                    self.vbox.show()
+            if event.keyval == Gdk.KEY_0:
+                if fpath.isfile("mychannels9.txt"):
+                    self.makeList(self.list_10)
+                    self.vbox.show()
+                # volume up
+            if event.keyval == Gdk.KEY_plus:
+                if self.mpv.volume < 205:
+                    self.mpv.volume += 5.0
+                    self.volume = self.mpv.volume
+                print(f"Volume: {self.mpv.volume}")
+                # volume down
+            if event.keyval == Gdk.KEY_minus: 
+                if self.mpv.volume >= 5:
+                    self.mpv.volume -= 5.0
+                    self.volume = self.mpv.volume
+                print(f"Volume: {self.mpv.volume}")
+                # toggle sidebar
+            if event.keyval == Gdk.KEY_s:
+                self.toggleSideBar()            
+            if event.keyval == Gdk.KEY_f or \
+                 (self.fullscreen and event.keyval == Gdk.KEY_Escape):
+                self.toggle_fullscreen()
+                # next
+            if self.is_playing:
+                if event.keyval == Gdk.KEY_Up:
+                    child = self.channelbox.get_child_at_index(self.id + 1)
+                    self.channelbox.select_child(child)
+                    if self.sidebar.is_visible():
+                        self.toggleSideBar()
+                    self.id += 1
+                    self.play_async(self.nameList[self.id], self.urlList[self.id])
+                    # previous
+                if event.keyval == Gdk.KEY_Down:
+                    child = self.channelbox.get_child_at_index(self.id - 1)
+                    self.channelbox.select_child(child)
+                    if self.sidebar.is_visible():
+                        self.toggleSideBar()
+                    self.id -= 1
+                    self.play_async(self.nameList[self.id], self.urlList[self.id]) 
             
     def toggle_fullscreen(self):
         self.fullscreen = (not self.fullscreen)
         if self.fullscreen:
             # Fullscreen
             self.win.fullscreen()
-            self.sidebar.hide()
+            self.vbox.hide()
         else:
             # Normal
             self.win.unfullscreen()
-            #self.sidebar.show()
+            #self.vbox.show()
             
     def toggleSideBar(self):
-        if self.sidebar.is_visible():
-            w = self.win.get_size()[0]
-            h = self.win.get_size()[1]
-            self.sidebar.hide()
+        if self.vbox.is_visible():
+            w, h = self.win.get_size()
+            self.vbox.hide()
             self.win.resize(h * 1.77, h)
         else:
-            w = self.win.get_size()[0]
-            h = self.win.get_size()[1]
-            self.sidebar.show()
+            w, h = self.win.get_size()
+            self.vbox.show()
             self.win.resize(h * 2.2 , h)
             
             
@@ -221,17 +241,21 @@ class MyWindow(Gtk.Window):
         if self.mpv != None:
             self.mpv.stop()
 
-        self.mpv = mpv.MPV(volume=str(self.volume), input_cursor=False, hwdec=False, 
-                                input_default_bindings=False, border=False, 
-                                input_vo_keyboard=False, osc=False, ontop=True, wid=str(self.mpv_player.get_window().get_xid()))
+        self.mpv = mpv.MPV(volume=str(self.volume), input_cursor=True, hwdec=False, 
+                                input_default_bindings=False, border=False, osd_color='#d3d7cf', 
+                                osd_blur=2, cursor_autohide=1000, 
+                                input_vo_keyboard=False, osc=False, ontop=True, 
+                                wid=str(self.mpv_player.get_window().get_xid()))
 
     def reinit_mpv_movies(self):
         if self.mpv != None:
             self.mpv.stop()
 
         self.mpv = mpv.MPV(volume=str(self.volume), input_cursor=True, hwdec=False, 
-                                input_default_bindings=False, border=False, 
-                                input_vo_keyboard=False, osc=True, ontop=True, wid=str(self.mpv_player.get_window().get_xid()))
+                                input_default_bindings=False, border=False, osd_color='#d3d7cf', 
+                                osd_blur=2, cursor_autohide=1000, 
+                                input_vo_keyboard=False, osc=True, ontop=True, 
+                                wid=str(self.mpv_player.get_window().get_xid()))
                                 
     def on_mpv_player_draw(self, widget, cr):
         cr.set_source_rgb(0.0, 0.0, 0.0)
@@ -259,28 +283,51 @@ class MyWindow(Gtk.Window):
             self.mpv.wait_until_playing()
             self.is_playing = True
             self.mpv.show_text(channelname, duration="3000", level=None)
+            self.channelbox.grab_focus()
             
     def makeList(self, mlist):
         for child in self.channelbox.get_children():
             self.channelbox.remove(child)
-
         text = open(mlist, 'r').read()
         self.urlList = []
         self.nameList = []      
         for lines in text.splitlines():
             line = lines.split(",")
-            self.nameList.append(line[0])
-            self.urlList.append(line[1])
+            name, url = line[0], line[1]
+            self.nameList.append(name)
+            self.urlList.append(url)
   
         i = 0
         for ch in self.nameList:
             btn = Gtk.Button(label = ch)
-            btn.set_name(f"btn{str(i)}")
-            btn.set_can_focus(True)
+            child = Gtk.FlowBoxChild()
+            child.set_name(ch)
+            child.add(btn)
             btn.connect("clicked", self.btn_clicked, i)
-            self.channelbox.add(btn)
+            self.channelbox.add(child)
             i += 1
         self.channelbox.show_all()
+        
+    def find_channel(self, *args):
+        text = self.searchbar.get_text()
+        if len(text) > 0:
+            self.flowbox_filter(text)
+        else:
+            self.flowbox_filter("")
+            self.channelbox.grab_focus()            
+        
+    def flowbox_filter(self, search_entry):
+        def filter_func(fb_child, text):
+            if text in fb_child.get_name() or text.lower() in fb_child.get_name() \
+            or text.upper() in fb_child.get_name() or text.title() in fb_child.get_name():
+                return True
+            else:
+                return False
+                
+        text = self.searchbar.get_text()        
+        self.channelbox.set_filter_func(filter_func, text)
+
+            
         
     def main(self, argv):
         self.list_1 = "mychannels1.txt"
@@ -301,7 +348,7 @@ class MyWindow(Gtk.Window):
         self.fullscreen = False
         self.id = 0
         builder = Gtk.Builder()
-        builder.add_from_file("myglade_mpv.glade")
+        builder.add_from_file("myglade_mpv2.glade")
 
         screen = Gdk.Screen.get_default()        
         css_provider = Gtk.CssProvider()
@@ -309,8 +356,7 @@ class MyWindow(Gtk.Window):
 
         context = Gtk.StyleContext()
         context.add_provider_for_screen(screen, css_provider,
-          Gtk.STYLE_PROVIDER_PRIORITY_USER)
-
+          Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
         self.mpv_player = builder.get_object("mpv_player")
         self.win = builder.get_object("window")
@@ -318,6 +364,12 @@ class MyWindow(Gtk.Window):
         self.win.connect('key_press_event', self.on_key_press_event)
         self.sidebar = builder.get_object("sidebar")
         self.channelbox = builder.get_object("channels_flowbox")
+        self.channelbox.set_can_focus(True)
+        self.searchbar = builder.get_object("searchbar")
+        self.searchbar.set_placeholder_text("find ...")
+        #self.searchbar.connect("stop_search", lambda: self.channelbox.show_all())
+        self.searchbar.connect('search_changed', self.find_channel)
+        self.vbox = builder.get_object("vbox")
         self.win.connect("destroy", Gtk.main_quit)
         self.win.connect('button_press_event', self.on_button_press_event)
         self.win.connect('scroll-event', self.my_zoom)
@@ -335,6 +387,7 @@ class MyWindow(Gtk.Window):
         self.win.resize(640, 300)
         self.win.move(50, 50)
         self.win.show_all()
+        self.channelbox.grab_focus()
         Gtk.main() 
 
 
